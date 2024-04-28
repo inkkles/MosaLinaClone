bdfc0c2b10506c694e0dab0beb5ca0bf68550e1e
inkkles <mauluzardo@gmail.com>
2024-04-28 06:50:00 -0400

    Added Levels!
    ADDED:
    - More basic blocks
    - A gravity affected 1x1 block as a base prefab (for variants to inherit)
    - Tutorial + 7/10 Levels
    - Plank (gravity affected map element)
    - Hinge
    (How to use hinge: Place hinge where you want the pivot point to be, then make the map element you want to be connected to the hinge a child of the hinge object in the hierarchy. Then, drag that child into the hinge's rigidbody slot in HingeJoint2D.)
    - TextMeshPro (for the tutorial text)
    
    REMOVED:
    - Old level 1 scene
    
    FIXED:
    - ShadowCast.cs !!!
    It works so good now im so happy, should be on everything that has a shadow in game, which is pretty much everything
    
    KNOWN BUGS:
    - Player can jump on the portal, therefore the player isn't ignoring that physics layer
    - GravBoots, just GravBoots.
    
    TODO:
    Player death and adding spikes

--- Assets/Scripts/ShadowCast.cs
+++ Assets/Scripts/ShadowCast.cs
@@ -8,20 +8,25 @@ public class ShadowCast : MonoBehaviour
     GameObject shadow;
     SpriteRenderer shadowSprite;
 
-    public float xOffset = -0.1f;
-    public float yOffset = -0.075f;
+    float xOffset;
+    float yOffset;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Game-wide shadow offsets
+        xOffset = -0.1f;
+        yOffset = -0.075f;
+
         //Spawn in game object with a transform component (redundent, transform components are mandatory)
         shadow = new GameObject("Dropshadow", typeof(Transform));
-        //Make the transform of the shadow dependent on the transform of whatever object this script is connected to
+        //Make the shadow a child of this object
         shadow.transform.parent = this.transform;
-        //Move shadow by public offset, with the Z at 1 to have it render behind everything else
-        shadow.transform.localPosition = new Vector3(xOffset, yOffset, 4);
-        //make scale identical
-        shadow.transform.localScale = this.transform.localScale;
+        //set the rotation of the shadow to the same rotation this object started as
+        shadow.transform.rotation = this.transform.rotation;
+        //match the scale
+        shadow.transform.localScale = Vector3.one;
+        
 
         //Add sprite renderer
         shadowSprite = shadow.AddComponent<SpriteRenderer>();
@@ -34,9 +39,8 @@ public class ShadowCast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Update the shadow's sprite to keep up with potential animation
-        shadowSprite.sprite = this.GetComponent<SpriteRenderer>().sprite;
-        shadow.transform.localPosition = new Vector3(xOffset, yOffset, 1);
+        //offset the shadow
+        shadow.transform.position = this.transform.position + new Vector3(xOffset, yOffset, 1);
 
     }
 }
